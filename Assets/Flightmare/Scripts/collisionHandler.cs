cd4edf05784f7e8884eca61da8b8cd35c1b9e2be
slimeth <snaji@student.ethz.ch>
2020-12-01 18:52:11 +0100

    fixed undetected collisions

--- Assets/Flightmare/Scripts/collisionHandler.cs
+++ Assets/Flightmare/Scripts/collisionHandler.cs
@@ -5,25 +5,50 @@ using UnityEngine;
 public class collisionHandler : MonoBehaviour
 {
   public bool hasCollided = false;
+  List<string> ignore = new List<string>(); // object that should not appear in pointcloud
   
-  // Start is called before the first frame update
-  // void Start() {}
+  //Start is called before the first frame update
+  void Start() {
+      //Unwanted objects for collision detection
+      ignore.Add("HDCamera");
+      ignore.Add("Drone_red");
+      ignore.Add("HDCamera(Clone)");
+      ignore.Add("Drone_red(Clone)");
+      ignore.Add("Transparent_Cube");
 
-  // Update is called once per frame
-  // void Update() {}
+      // get all colliders and change them to trigger
+       var colls = FindObjectsOfType<Collider>();
+       foreach (var coll in colls)
+       {
+           coll.isTrigger = true;
+       }
 
-  private void OnTriggerEnter(Collider collision)
-  {
-      hasCollided = true;
   }
 
-  private void OnTriggerStay(Collider collision)
+  //Update is called once per frame
+  void Update() {}
+
+  private void OnTriggerEnter(Collider other)
   {
-      hasCollided = true;
+    if (!ignore.Contains(other.gameObject.name))
+    {
+        hasCollided = true;
+    }
   }
 
-  private void OnTriggerExit(Collider collision)
+  private void OnTriggerStay(Collider other)
   {
-      hasCollided = false;
+    if (!ignore.Contains(other.gameObject.name))
+    {
+        hasCollided = true;
+    }
+  }
+
+  private void OnTriggerExit(Collider other)
+  {
+    if (!ignore.Contains(other.gameObject.name))
+    {
+        hasCollided = false;
+    }
   }
 }
